09a9c7750e35ee2b27c06c116021cf6267144c8f
cyri113 <c.derche@me.com>
2025-03-17 17:31:16 +0100

    hide connect button if website platform exists

--- src/components/communitySettings/communityPlatforms/TcWebsite.tsx
+++ src/components/communitySettings/communityPlatforms/TcWebsite.tsx
@@ -112,20 +112,21 @@ function TcWebsite({
 
 	return (
 		<div className="flex items-center space-x-3 rounded-sm bg-secondary bg-opacity-5 p-5">
-			<Paper className="flex h-[6rem] w-[10rem] flex-col items-center justify-center rounded-sm py-2 shadow-none">
-				<span className="mx-auto">
-					<TcCommunityPlatformIcon platform="Website" size={40} />
-				</span>
-				<div className="mx-auto w-10/12 text-center">
-					<TcButton
-						text="Connect"
-						variant="text"
-						color="primary"
-						startIcon={<BiPlus />}
-						onClick={() => handleOpenDialog()}
-					/>
-				</div>
-			</Paper>
+			{connectedPlatforms.length === 0 ?
+				<Paper className="flex h-[6rem] w-[10rem] flex-col items-center justify-center rounded-sm py-2 shadow-none">
+					<span className="mx-auto">
+						<TcCommunityPlatformIcon platform="Website" size={40} />
+					</span>
+					<div className="mx-auto w-10/12 text-center">
+						<TcButton
+							text="Connect"
+							variant="text"
+							color="primary"
+							startIcon={<BiPlus />}
+							onClick={() => handleOpenDialog()}
+						/>
+					</div>
+				</Paper> : <></>}
 			{isLoading ? (
 				<CircularProgress size={30} />
 			) : (
