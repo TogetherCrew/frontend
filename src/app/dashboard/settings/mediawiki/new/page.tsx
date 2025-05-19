8f6568990fd47ace55a8463b69bb3f3ce61d85d7
cyri113 <c.derche@me.com>
2025-05-19 16:49:17 +0200

    remove breadcrumbs from MW /new

--- src/app/dashboard/settings/mediawiki/new/page.tsx
+++ src/app/dashboard/settings/mediawiki/new/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import Breadcrumbs from "@/components/layouts/Breadcrumbs";
 import MediaWikiForm from "@/components/mediawiki/Form";
 
 export default function MediaWikiNewPage() {
   return (
     <div className="flex flex-col gap-4">
-      <Breadcrumbs />
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold">New MediaWiki</h1>
       </div>
